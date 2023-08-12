db024592edae33031451533e03242fda23b85f5d
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-08-12 18:37:58 +0200

    unit-tests(app): add unit test for App file

---
+++ src/App.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {describe, expect, it, jest} from '@jest/globals';
+import {render, waitFor} from '@testing-library/react-native';
+import App from './App';
+import {Linking} from 'react-native';
+import {ALLOWED_LINKS} from './types';
+
+// Mock the ServicesManager
+jest.mock('./modules/services/ServicesManager', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      init: jest.fn(() => Promise.resolve()), // Return a promise that resolves immediately.
+    };
+  });
+});
+
+// Mock crashlytics and Linking
+jest.mock('@react-native-firebase/crashlytics', () => {
+  return () => ({
+    log: jest.fn(),
+    crash: jest.fn(),
+  });
+});
+
+describe('App', () => {
+  it('shows "Initializing application services" when app is not ready', async () => {
+    const {getByText} = render(<App />);
+    await waitFor(() => {
+      expect(getByText('Initializing application services')).toBeTruthy();
+    });
+  });
+  it('should fake universal links implementation for Login Screen', async () => {
+    const initialUrlMock = jest
+      .spyOn(Linking, 'getInitialURL')
+      .mockResolvedValue(ALLOWED_LINKS.AUTH_ENDPOINT);
+    expect(initialUrlMock).toHaveBeenCalled();
+
+    const {getByText} = render(<App />);
+    // Wait for the promise and state updates to be reflected in the UI
+    await waitFor(() => {
+      expect(getByText('Hello from Login screen')).toBeTruthy();
+    });
+  });
+});
