6ad8fad8716ad6f077c8a5299c68c942e0e47547
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-08-13 18:54:00 +0200

    chore(package.json): add new e2e scripts for different environement

--- .detoxrc.js
+++ .detoxrc.js
@@ -17,6 +17,20 @@ module.exports = {
       build:
         'xcodebuild -workspace ios/ReactNativeStarterkit.xcworkspace -scheme ReactNativeStarterkit-Development -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
     },
+    'ios.staging.debug': {
+      type: 'ios.app',
+      binaryPath:
+        'ios/build/Build/Products/Debug-iphonesimulator/ReactNativeStarterkit-Staging.app',
+      build:
+        'xcodebuild -workspace ios/ReactNativeStarterkit.xcworkspace -scheme ReactNativeStarterkit-Staging -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
+    },
+    'ios.prod.debug': {
+      type: 'ios.app',
+      binaryPath:
+        'ios/build/Build/Products/Debug-iphonesimulator/ReactNativeStarterkit.app',
+      build:
+        'xcodebuild -workspace ios/ReactNativeStarterkit.xcworkspace -scheme ReactNativeStarterkit -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
+    },
     'ios.dev.release': {
       type: 'ios.app',
       binaryPath:
@@ -30,6 +44,22 @@ module.exports = {
         'android/app/build/outputs/apk/Development/debug/app-Development-debug.apk',
       build:
         'cd android && ENVFILE=.env.dev ./gradlew assembleDevelopment assembleAndroidTest -DtestBuildType=debug',
+      reversePorts: [8081],
+    },
+    'android.staging.debug': {
+      type: 'android.apk',
+      binaryPath:
+        'android/app/build/outputs/apk/Staging/debug/app-Staging-debug.apk',
+      build:
+        'cd android && ENVFILE=.env.staging ./gradlew assembleStaging assembleAndroidTest -DtestBuildType=debug',
+      reversePorts: [8081],
+    },
+    'android.prod.debug': {
+      type: 'android.apk',
+      binaryPath:
+        'android/app/build/outputs/apk/Production/debug/app-Production-debug.apk',
+      build:
+        'cd android && ENVFILE=.env.prod ./gradlew assembleProduction assembleAndroidTest -DtestBuildType=debug',
       reversePorts: [8081],
     },
     'android.dev.release': {
@@ -60,9 +90,17 @@ module.exports = {
     },
   },
   configurations: {
-    'ios.sim.debug': {
+    'ios.sim.dev.debug': {
       device: 'simulator',
       app: 'ios.dev.debug',
+    },
+    'ios.sim.staging.debug': {
+      device: 'simulator',
+      app: 'ios.staging.debug',
+    },
+    'ios.sim.prod.debug': {
+      device: 'simulator',
+      app: 'ios.prod.debug',
     },
     'ios.sim.release': {
       device: 'simulator',
@@ -76,9 +114,17 @@ module.exports = {
       device: 'attached',
       app: 'android.dev.release',
     },
-    'android.emu.debug': {
+    'android.emu.dev.debug': {
       device: 'emulator',
       app: 'android.dev.debug',
+    },
+    'android.emu.staging.debug': {
+      device: 'emulator',
+      app: 'android.staging.debug',
+    },
+    'android.emu.prod.debug': {
+      device: 'emulator',
+      app: 'android.prod.debug',
     },
     'android.emu.release': {
       device: 'emulator',
